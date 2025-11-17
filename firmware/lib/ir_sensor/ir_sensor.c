261ccb4b87d27391d976aa2b01d80a2b7a510d0b
Jadden Picardal <jaddenki@gmail.com>
2025-11-17 16:01:23 -0500

    bb test

--- firmware/lib/ir_sensor/ir_sensor.c
+++ firmware/lib/ir_sensor/ir_sensor.c
@@ -1,25 +1,37 @@
 #include "ir_sensor.h"
+#include "hardware/irq.h"
 #include <stdio.h>
 
-static ir_sensor_callback_t user_callback = NULL;
+static ir_sensor_callback_t callbacks[32] = {NULL};
+static bool irq_bank_enabled = false;
 
-static void ir_sensor_irq_handler(uint gpio, uint32_t events) {
-    if (user_callback == NULL) return;
-    
-    bool is_rising = (events & GPIO_IRQ_EDGE_RISE) != 0;
-    user_callback(gpio, is_rising);
+static void ir_sensor_irq_handler(void) {
+    for (uint gpio = 0; gpio < 32; gpio++) {
+        if (callbacks[gpio] == NULL) continue;
+        
+        uint32_t events = gpio_get_irq_event_mask(gpio);
+        if (events) {
+            gpio_acknowledge_irq(gpio, events);
+            bool is_rising = (events & GPIO_IRQ_EDGE_RISE) != 0;
+            callbacks[gpio](gpio, is_rising);
+        }
+    }
 }
 
 void ir_sensor_init(uint gpio_pin, ir_sensor_callback_t callback) {
-    user_callback = callback;
+    callbacks[gpio_pin] = callback;
     
     gpio_init(gpio_pin);
     gpio_set_dir(gpio_pin, GPIO_IN);
     
-    gpio_set_irq_enabled_with_callback(gpio_pin, 
-                                       GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, 
-                                       true, 
-                                       &ir_sensor_irq_handler);
+    if (!irq_bank_enabled) {
+        irq_set_enabled(IO_IRQ_BANK0, true);
+        irq_bank_enabled = true;
+    }
+    
+    gpio_add_raw_irq_handler(gpio_pin, ir_sensor_irq_handler);
+    
+    gpio_set_irq_enabled(gpio_pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
 }
 
 bool ir_sensor_read(uint gpio_pin) {
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -7,15 +7,11 @@
 #include "bibi_config.h"
 
 void run_animation_demo(ili9341_t *display);
-void test_face_with_ir_sensor(ili9341_t *display, uint gpio_pin);
+void test_face_with_ir_sensor();
 
 int main() {
     stdio_init_all();
-    
-    ili9341_t display;
-    ili9341_init(&display, spi1, DISPLAY_PIN_CS, DISPLAY_PIN_DC, DISPLAY_PIN_RST, DISPLAY_PIN_MOSI, DISPLAY_PIN_SCK);
-    
-    test_face_with_ir_sensor(&display, 35);
+    test_face_with_ir_sensor();
     
     return 0;
 }
